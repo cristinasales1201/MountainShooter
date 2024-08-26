16652fb6c9fa9d3aa68ad18abd383fa7d3f3ebc6
Cristina Sales <112973264+cristinasales1201@users.noreply.github.com>
2024-08-26 15:27:35 -0300

    Enemy 3 se movimentando na vertical, porém preciso ajustar velocidade da subida. E corrigir problema do score.

--- code/Enemy.py
+++ code/Enemy.py
@@ -10,8 +10,23 @@ class Enemy(Entity):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
+    #Movimento vertical Enemy 3
+        self.movement_vertical = name == 'Enemy3'
+        if self.movement_vertical:
+            self.inicio_y = position [1]
+            #Velocidade do enemy 3
+            self.y_velocidade = 2
+            self.direction = 1
+            self.y_max = position[1] + 100
+            self.y_min = position[1] - 100
+
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+        #Movimento do enemy 3 na vertical
+        if self.movement_vertical:
+            if self.rect.centery >= self.y_max or self.rect.centery <= self.y_min:
+                self.direction *= -1 # para inverter a direção
+            self.rect.centery += self.y_velocidade * self.direction
 
     def shoot(self):
         self.shot_delay -= 1
