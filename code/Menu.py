112e9ce2e17c889a21a665684e7bba26c82f6244
Cristina Sales <112973264+cristinasales1201@users.noreply.github.com>
2024-08-26 10:06:30 -0300

    Inserido nome e RU no Menu.

--- code/Menu.py
+++ code/Menu.py
@@ -5,7 +5,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW, C_BLACK
 
 
 class Menu:
@@ -23,6 +23,7 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(9, "Ana Cristina S N Xavier - RU: 4693405", C_BLACK, (100, 315))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
